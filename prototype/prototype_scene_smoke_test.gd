171b6347d4962de0d00f02486bb32c6b7dfe4062
shafranovskijm-pixel <shafranovskij.m@gmail.com>
2026-08-09 18:57:09 +1000

    Make scene smoke test release-safe

--- prototype/prototype_scene_smoke_test.gd
+++ prototype/prototype_scene_smoke_test.gd
@@ -1,6 +1,7 @@
 extends SceneTree
 
 const Prototype = preload("res://prototype/prototype_v03.gd")
+const CI_MARKER = "PROTOTYPE_V03_SCENE_SMOKE_OK"
 
 func _init():
     var p = Prototype.new()
@@ -36,7 +37,8 @@ func _init():
     var impossible = p.attempt_free_action("купить хлеб")
     assert(not bool(impossible["ok"]))
 
-    print("PROTOTYPE_V03_SCENE_SMOKE_OK")
+    p.free()
+    _mark_success(CI_MARKER)
     quit(0)
 
 func _npc_by_id(p, npc_id):
@@ -44,3 +46,11 @@ func _npc_by_id(p, npc_id):
         if str(n["id"]) == npc_id:
             return n
     return {}
+
+func _mark_success(marker):
+    var marker_path = "res://.ci_%s.ok" % marker
+    var file = FileAccess.open(marker_path, FileAccess.WRITE)
+    assert(file != null)
+    file.store_string(marker)
+    file.close()
+    print(marker)
