055c4c10655f181161042077e8cd11782cf7e715
shafranovskijm-pixel <shafranovskij.m@gmail.com>
2026-08-09 13:20:19 +1000

    Use real production economy in housing health smoke test

--- scripts/smoke_test_v22.gd
+++ scripts/smoke_test_v22.gd
@@ -2,6 +2,7 @@ extends SceneTree
 
 const HousingSystem=preload("res://scripts/housing_system.gd")
 const HealthLifecycle=preload("res://scripts/health_lifecycle.gd")
+const ProductionEconomy=preload("res://scripts/production_economy.gd")
 
 func _init():
     var housing=HousingSystem.new();housing.setup()
@@ -13,11 +14,8 @@ func _init():
     npcs=housing.tick(npcs,1,9.0)
     assert(str(npcs[0].get("home_id",""))!="")
     assert(bool(npcs[1].get("homeless",false)) or str(npcs[1].get("home_id",""))!="")
-    var production=RefCounted.new()
-    production.set("hunger_pressure",0.0)
-    production.set("resources",{"medicine":5.0})
-    var life=HealthLifecycle.new()
-    npcs=life.tick(npcs,production,2,7.0)
+    var production=ProductionEconomy.new();production.hunger_pressure=0.0;production.resources["medicine"]=5.0
+    var life=HealthLifecycle.new();npcs=life.tick(npcs,production,2,7.0)
     assert(npcs[0].has("health"))
     print("V22 HOUSING/HEALTH SMOKE TEST OK")
     quit(0)
