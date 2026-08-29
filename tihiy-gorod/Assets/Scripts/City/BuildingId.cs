fbdf70fb91aa78c39ce665dfdb7ce7f414c5cad6
shafranovskijm-pixel <shafranovskij.m@gmail.com>
2026-08-29 19:58:03 +1000

    Add Тихий город sources (batch 1 metas and small files)

---
+++ tihiy-gorod/Assets/Scripts/City/BuildingId.cs
@@ -0,0 +1,15 @@
+namespace TihiyGorod
+{
+    public enum BuildingId
+    {
+        None = 0,
+        Sanctuary = 1,
+        Garden = 2,
+        Crypt = 3,
+        ShadowTower = 4,
+        Observatory = 5,
+        Crystal = 6,
+        MushroomRing = 7,
+        FairyHouse = 8
+    }
+}
---
+++ tihiy-gorod/Assets/Scripts/Units/UnitAiState.cs
@@ -0,0 +1,22 @@
+namespace TihiyGorod
+{
+    public enum UnitAiState
+    {
+        Idle = 0,
+        Wander = 1,
+        Travel = 2,
+        Gather = 3,
+        GoHome = 4,
+        Shelter = 5,
+        Sit = 6,
+        Drink = 7
+    }
+
+    public enum UnitKind
+    {
+        Villager = 0,
+        Spirit = 1,
+        Wisp = 2,
+        Cat = 3
+    }
+}
